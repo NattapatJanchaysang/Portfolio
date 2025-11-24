4f5414336299ad28d603c3085586ade52cfa5240
NattapatJanchaysang <66011360@kmitl.ac.th>
2025-11-25 00:37:14 +0700

    Finally fixed Tailwind bug

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,8 +1,41 @@
-import { useState } from 'react';
 import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export const ThemeToggle = () => {
-    const [isLightMode, setisLightMode] = useState('false');
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-    return <button>{isLightMode ? <Moon /> : <Sun />}</button>
-}
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+    >
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
+};
